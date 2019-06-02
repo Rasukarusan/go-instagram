4c3bb332d482437b2b9cb546a9d966eff1c96176
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-06-03 00:16:01 +0900

    エンドポイント生成、InstagramにCurl、レスポンス実装

---
+++ api/client.go
@@ -0,0 +1,30 @@
+package api
+
+import(
+    "strings"
+    "regexp"
+    "encoding/json"
+)
+
+/**
+ * HTML内のJSONをgrepの要領で抜き出しており、io.Reader型ではなくJSON文字列で処理するため、
+ * json.NewDecoderではなくjson.Unmarshalを使用する
+ */
+func (c Client) DecodeHTMLToJSON(html string) (*InstagramResponse, error) {
+    r := regexp.MustCompile(`window._sharedData = {.*}`)
+    jsonStr := strings.Replace(r.FindStringSubmatch(html)[0], "window._sharedData = ", "", 1)
+    bytes := []byte(jsonStr)
+    var response InstagramResponse
+    err := json.Unmarshal(bytes, &response)
+    if err != nil {
+        return nil, err
+    }
+    return &response, err
+}
+
+/**
+ * @TODO 空実装
+ */
+type Client struct{}
+func (c Client) NewClient(){}
+func (c Client) NewRequest(){}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module instagram
+
+require github.com/ant0ine/go-json-rest v3.3.2+incompatible
